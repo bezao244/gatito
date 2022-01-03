16315b107bb024325dfc379c7bc0ec5153bbd69f
Bernardo Afonso <bernardoafonso9734@gmail.com>
2022-01-03 19:43:12 -0300

    FInalizando a página

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 import { Carrosel } from "./carrosel.js";
 
-new Carrosel();
+const anterior = '[data-anterior]';
+const proximo = '[data-proximo]';
+const listaProdutos = '[data-lista]';
+const navegacao = '[data-navegacao]';
+
+new Carrosel(anterior, proximo, listaProdutos, navegacao);
